aecfd4757fa475382fc8f1c0d2706619622dd003
Mansi0905 <jaipur6696@gmail.com>
2026-06-22 23:34:48 +0530

    08-functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+   {/* <button onChange={()=>{
+    console.log("cluckckculck");
+   }}>clickkro</button>
+ */}
+
+{/* <input onChange={(elem)=>{
+  console.log(elem.target.value);
+  
+}} type="text" placeholder='name'/> */}
+
+
+
+{/* <div
+ onMouseMove={(elem)=>{
+  console.log(elem.clientX,elem.clientY);
+ }}
+  className="box"></div> */}
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
