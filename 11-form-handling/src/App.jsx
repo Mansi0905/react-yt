1206ca384ad0e1e0213a4a071a7f5b62003aafe7
Mansi0905 <jaipur6696@gmail.com>
2026-06-27 23:18:15 +0530

    11-form handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const App = () => {
+
+  // const submitHandler=(elem)=>{
+  //   elem.preventDefault()
+
+  //   console.log('submitted');
+    
+  // }
+
+
+
+
+  return (
+    <div>
+   {/* <form onSubmit={(elem)=>{
+      submitHandler(elem)
+     }}>
+      <input type="text" placeholder='enter yout name' />
+      
+      <button>submit</button>
+     </form> */}
+
+
+
+    </div>
+  )
+}
+
+export default App
