c3fd6d20c9e3e496f590e01e9949c2081d21a54e
Mansi0905 <jaipur6696@gmail.com>
2026-06-27 21:18:43 +0530

    yo-yo

--- 10-advnace-useState/src/App.jsx
+++ 10-advnace-useState/src/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
 
-  const [num, setnum] = useState(10)
+  // const [num, setnum] = useState({user:'Sarthak',age:55})
 
-  
+  // const btclicked=()=>{
+  //   const  newNum={...num};
+  //   newNum.user='Aman'
+  //   console.log(newNum);
+  //   console.log(num);
+
+  //   setnum(newNum)
+    
+  // }
+
+//  const [num, setnum] = useState(10)
+
+//  const btnclicked=()=>{
+//   setnum(prev =>(prev+1))
+//     setnum(prev =>(prev+1))
+
+//       setnum(prev =>(prev+1))
+
+//  }
+
   return (
     <div>
       <h1>{num}</h1>
-      <button>Click</button>
+      <button onClick={btnclicked}>Click</button>
     </div>
   )
 }
 
-export default App
+export default App  
