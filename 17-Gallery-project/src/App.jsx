9ab112ab459ecb770abef655f65408609dbc30ec
Mansi0905 <jaipur6696@gmail.com>
2026-07-14 23:33:36 +0530

    Add gallery project files

---
+++ 17-Gallery-project/src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const getData= async ()=>{
+    const response= await axios.get(' https://picsum.photos/v2/list?page=3&limit=20')
+
+    setUserData(response.data); 
+    
+  
+  }
+
+  let printUserData='no user available';
+  if(userData.length > 0){
+    printUserData=userData.map(function(elem,idx){
+      return <div key={idx}>
+        <a href={elem.url}>
+          <div className='h-40 overflow-hidden w-44 rounded-xl bg-white'>
+        <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
+      </div>
+      <h2 className='font-bold text-xl '>{elem.author}</h2>
+     
+        </a>
+         </div>
+    })
+  }
+  return (
+    <div className='bg-black overflow-auto h-screen text-white'>
+
+      <button
+      onClick={getData}
+       className='bg-green-400 active:scale-95  mb-3 px-5 py-2 cursor-pointer text-black '>
+        GetData
+        </button>
+
+      <div className='flex flex-wrap gap-5'>
+        {printUserData}
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
