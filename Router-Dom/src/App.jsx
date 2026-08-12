30b7838aa8978ae9abb33c39a94b7c1369f1bde3
Mansi0905 <jaipur6696@gmail.com>
2026-08-13 01:06:46 +0530

    dom complete

--- Router-Dom/src/App.jsx
+++ Router-Dom/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Product from './pages/Product'
 import Navbar from './components/Navbar'
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       <Route path = '/home' element = {<Home/>}/>
       <Route path = '/about' element = {<About/>}/>
       <Route path = '/contact' element ={<Contact/>}/>
-      
+      <Route path = '/product' element ={<Product/>}/>
 
     </Routes>
 
