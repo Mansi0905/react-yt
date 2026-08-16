8b425d438a31ab5d8b552244f9b6597c81124538
Mansi0905 <jaipur6696@gmail.com>
2026-08-16 22:01:57 +0530

    pagination of the components

--- 19-Router-Advanced/src/App.jsx
+++ 19-Router-Advanced/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
       <Navbar/>
+
+      
       <Footer/>
     </div>
   )
--- 19-Router-Advanced/src/components/Navbar.jsx
+++ 19-Router-Advanced/src/components/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex py-4 px-8 bg-cyan-900 justify-between'>
+      <h2 className='text-xl font-bold'>Rajasthali</h2>
+      <div className='flex gap-8'>
+        <a className='text-xl font-bold' href="/">Home</a>
+        <a className='text-xl font-bold' href="/about">About</a>
+        <a  href="/contact">Contact</a>
+
+      </div>
+    </div>
   )
 }
 
