2058bac2a0a76b8c324e8b9805596dde1f59aaff
Mansi0905 <jaipur6696@gmail.com>
2026-08-18 23:26:36 +0530

    Router dom advance- nested routes

--- 19-Router-Advanced/src/App.jsx
+++ 19-Router-Advanced/src/App.jsx
@@ -3,12 +3,23 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import {Route, Routes} from 'react-router-dom'
+import Notfound from './pages/Notfound'
 
 const App = () => {
   return (
     <div className='h-screen bg-black text-white'>
       <Navbar/>
-
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      <Route path ='*' element={<Notfound/>}/>
+      
+      </Routes>
   
       <Footer/>
     </div>
--- 19-Router-Advanced/src/components/Navbar.jsx
+++ 19-Router-Advanced/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex py-4 px-8 bg-cyan-900 justify-between'>
       <h2 className='text-3xl font-bold'>Rajasthali</h2>
-      <div className='flex gap-8'>
-        <a className='text-xl font-bold' href="/">Home</a>
-        <a className='text-xl font-bold' href="/about">About</a>
-        <a  href="/contact">Contact</a>
+      <div className='flex gap-10'>
+        
+        <Link className='text-xl font-medium' to='/'>Home</Link>
+        <Link className='text-xl font-medium' to='/about'>About</Link>
+        <Link className='text-xl font-medium' to='/contact'>Contact</Link>
+        <Link className='text-xl font-medium' to='/product'>Product</Link>
+
+        
 
       </div>
     </div>
