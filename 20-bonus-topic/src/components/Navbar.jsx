27bc5261f05c8cca9dd2b7d468227472cda320fd
Mansi0905 <jaipur6696@gmail.com>
2026-08-30 20:39:21 +0530

    passing props from child to parent

---
+++ 20-bonus-topic/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = (props) => {
+    console.log(props);
+    
+  return (
+    <div>
+        <button>Change theme</button>
+    </div>
+  )
+}
+
+export default Navbar
